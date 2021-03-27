887aa354862e2d327e6dedcb67ccd031d7739bd9
chengzhenguo1 <943944010@qq.com>
2021-03-27 16:23:13 +0800

    添加切换歌曲功能

--- src/components/Layout/Footer/PlayAtions/index.tsx
+++ src/components/Layout/Footer/PlayAtions/index.tsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { observer } from 'mobx-react'
 import {
  StepForwardOutlined, StepBackwardOutlined, CaretRightOutlined, PauseOutlined, 
 } from '@ant-design/icons'
-import useStores from '../../../../hooks/useStores'
+import useStores from '~/hooks/useStores'
 
 import './index.less'
 
 const PlayAtions: React.FC = () => {
     const { Music } = useStores()
-    const handelPlayMusic = (flag:boolean) => {
+    const {
+ audioInfo, controls, currentSong, playList, 
+} = Music
+    const togglePlayStatus = useCallback((flag:boolean) => {
         Music.setMusicState(flag)
-    }
+    }, [audioInfo?.paused, controls])
+
+    const play = useCallback((prev?:boolean) => {
+        const len = playList.length
+        if (len < 1) {
+            return
+        }
+        const index = playList.findIndex(({ musicId }) => currentSong?.musicId === musicId)
+        let nextIndex = -1
+        if (index > -1) {
+            nextIndex = prev ? (index - 1 + len) % len : (index + 1) % len
+        } else {
+            nextIndex = 0
+        }
+        Music.playListMusic(nextIndex)
+    }, [playList, currentSong?.musicId])
+
     return (
         <div className='play-ations'>
-            <StepBackwardOutlined />
+            <StepBackwardOutlined onClick={() => play()} />
             <div className='play-control'>
                 {Music.audioInfo.paused
-                ? <CaretRightOutlined onClick={(e) => handelPlayMusic(true)} /> 
-                : <PauseOutlined onClick={(e) => handelPlayMusic(false)} />}
+                ? <CaretRightOutlined onClick={(e) => togglePlayStatus(true)} /> 
+                : <PauseOutlined onClick={(e) => togglePlayStatus(false)} />}
             </div>
-            <StepForwardOutlined />
+            <StepForwardOutlined onClick={() => play(true)} />
         </div>
 )
 }
